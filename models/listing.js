6f64a1dbae064b4031f8fef856fb9627140819fe
AmanSahoo <amansahoo4567@gmail.com>
2024-08-23 11:50:18 +0530

    changes on navbar and footer

--- models/listing.js
+++ models/listing.js
@@ -1,31 +1,3 @@
-// //pre-defined schema
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//   },
-//   description: String,
-//   image: {
-//     type: String,
-//     default:
-//       "https://images.unsplash.com/photo-1722237959317-4ecde90e718b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D",
-//     set: (v) =>
-//       v === ""
-//         ? "https://images.unsplash.com/photo-1722237959317-4ecde90e718b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"
-//         : v,
-//   },
-//   price: Number,
-//   location: String,
-//   country: String,
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-
-// module.exports = Listing;
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
